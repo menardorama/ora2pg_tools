1ca7bb03e8687b8bc0a41c6c4507db1648982571
Thomas Menard <thomas@fr-tech-tmenard.emvailvision.com>
2014-05-23 17:08:53 +0200

    Add a crappy option --table_name to filter correctly on the oracle catalog

--- scripts/ora2pg_wrapper.sh
+++ scripts/ora2pg_wrapper.sh
@@ -33,7 +33,7 @@ echo "ALLOW ${TABLE_NAME}" >> $WORK_DIR/ora2pg.conf
 
 # Exporting DDL
 echo "$(date) : ${TABLE_NAME} : Begin"
-ora2pg -t TABLE -c $WORK_DIR/ora2pg.conf -o create_table.sql -b ${WORK_DIR} 
+ora2pg -t TABLE -c $WORK_DIR/ora2pg.conf -o create_table.sql -b ${WORK_DIR} --table_name ${TABLE_NAME}
 
 sed -i "s/TRUNC(LOCALTIMESTAMP, 'MONTH')/date_trunc('month', LOCALTIMESTAMP)/g ; s/trunc(LOCALTIMESTAMP, 'MONTH')/date_trunc('month', LOCALTIMESTAMP)/g" ${WORK_DIR}/create_table.sql
 
@@ -51,5 +51,6 @@ EOF
 fi
 
 
-ora2pg -t COPY -c $WORK_DIR/ora2pg.conf  -j 10
+ora2pg -t COPY -c $WORK_DIR/ora2pg.conf  -j 10 --table_name ${TABLE_NAME}
+
 echo "$(date) : ${TABLE_NAME} : End"
